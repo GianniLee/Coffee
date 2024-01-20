03f15f6605c9f7f5f41e7a98fee4bfba35d2bb97
Jian Lee <jalee0201@kaist.ac.kr>
2024-01-20 14:44:37 +0900

    Initial commit

---
+++ Coffee_server/src/test/java/MadcampWeek4/Coffee/CoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package MadcampWeek4.Coffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
